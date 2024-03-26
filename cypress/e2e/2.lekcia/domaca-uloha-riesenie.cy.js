50bee46fd37ad7090e0fed49bf8b09f1e4f08591
martin <skarbala.martin@gmail.com>
2024-03-26 14:56:45 +0100

    adds lecture 2 teaching materials

---
+++ cypress/e2e/2.lekcia/domaca-uloha-riesenie.cy.js
@@ -0,0 +1,48 @@
+describe("Gringottbank", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:8080/#/gringottsBank")
+    })
+
+    it("display message when investment list is empty", () => {
+        cy.get('.no-investments-message')
+            .should('be.visible')
+            .and('have.text', 'No investments yet. Make your first one!')
+    })
+
+    it("create multiple investments", () => {
+        for (let index = 0; index < 2; index++) {
+            cy.get("#selectedFund").select("Phoenix Feather Portfolio")
+            cy.get("#oneTimeInvestment").type(15000)
+            cy.get("#years").type(12)
+            cy.get("[data-test='create-offer']").click()
+            cy.get("[data-test='customer-name']").type('Jozo')
+            cy.get('[data-test="create-investment"]').click()
+        }
+
+        cy.get('ul.investment-list')
+            .find('li')
+            .should('have.length', 2)
+    })
+
+    it.only("delete an investment", () => {
+        cy.get("#selectedFund").select("Phoenix Feather Portfolio")
+        cy.get("#oneTimeInvestment").type(15000)
+        cy.get("#years").type(12)
+        cy.get("[data-test='create-offer']").click()
+        cy.get("[data-test='customer-name']").type('Jozo')
+        cy.get('[data-test="create-investment"]').click()
+
+        cy.get('ul.investment-list')
+            .find('li')
+            .contains('button', 'Delete')
+            .click()
+
+        cy.get('ul.investment-list')
+            .find('li')
+            .should('have.length', 0)
+
+        cy.get('.no-investments-message')
+            .should('be.visible')
+            .and('have.text', 'No investments yet. Make your first one!')
+    })
+})
---
+++ cypress/e2e/2.lekcia/domaca-uloha-zadanie.md
@@ -0,0 +1,7 @@
+# Domáca úloha - 2. Lekcia
+
+### Gringott Banka
+napíšte automatické testy, ktoré overia
+1. zobrazenie hlášky `No investments yet. Make your first one!` ak je zoznam investícií prázdny
+2. zadanie viacerých investícií
+3. vymazanie investície kliknutím na tlačidlo Delete
