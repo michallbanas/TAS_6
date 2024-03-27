1cb42c812fe30218b4e72a79300cc49be5876f71
michalbanas <banas.michal@icloud.com>
2024-03-27 08:18:47 +0100

    chore: 5 lecture

--- cypress/e2e/2.lekcia/02-lekcia-finish.cy.js
+++ cypress/e2e/2.lekcia/02-lekcia-finish.cy.js
@@ -1,60 +1,57 @@
 describe("Gringottbank", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:8080/#/gringottsBank")
-    })
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/#/gringottsBank")
+  })
 
-    it("display offer data", () => {
-        cy.log("select fund")
-        //zadanie vstupnych hodnot
-        cy.get("[id=selectedFund]").select("Phoenix Feather Portfolio")
-        cy.get("[id=oneTimeInvestment]").type(10000)
-        cy.get("#years").type(10)
-        //vyhladanie elementu podla dedikovaneho data-test atributu
-        cy.get("[data-test='create-offer']").click()
-        //vyhladanie elementu podla typu a textu
-        cy.contains('button', 'Make me an offer').click()
+  it("display offer data", () => {
+    cy.log("select fund")
+    //zadanie vstupnych hodnot
+    cy.get("[id=selectedFund]").select("Phoenix Feather Portfolio")
+    cy.get("[id=oneTimeInvestment]").type(10000)
+    cy.get("#years").type(10)
+    //vyhladanie elementu podla dedikovaneho data-test atributu
+    cy.get("[data-test='create-offer']").click()
+    //vyhladanie elementu podla typu a textu
+    cy.contains("button", "Make me an offer").click()
 
-        cy.get("div.offer-detail").should('be.visible')
+    cy.get("div.offer-detail").should("be.visible")
 
-        cy.get("div.offer-detail")
-            .find('.your-data')
-            .find('p.period')
-            .find('span')
-            .should('not.be.empty')
-            .and('contain.text', '10 years')
+    cy.get("div.offer-detail")
+      .find(".your-data")
+      .find("p.period")
+      .find("span")
+      .should("not.be.empty")
+      .and("contain.text", "10 years")
 
-        //vyhladanie fund podla textu elementu
-        cy.get("div.offer-detail")
-            .find('.your-data')
-            .contains('p', 'Fund')
-            .find('span')
-            .should('not.be.empty')
-            .and('contain.text', 'Phoenix Feather Portfolio')
-    })
+    //vyhladanie fund podla textu elementu
+    cy.get("div.offer-detail")
+      .find(".your-data")
+      .contains("p", "Fund")
+      .find("span")
+      .should("not.be.empty")
+      .and("contain.text", "Phoenix Feather Portfolio")
+  })
 
-    it.only("correct name is stored in new investment", () => {
-        cy.get("#selectedFund").select("Phoenix Feather Portfolio")
-        cy.get("#oneTimeInvestment").type(15000)
-        cy.get("#years").type(12)
-        cy.get("[data-test='create-offer']").click()
-        cy.get("[data-test='customer-name']").type('Jozo')
-        cy.get('[data-test="create-investment"]').click()
+  it.only("correct name is stored in new investment", () => {
+    cy.get("#selectedFund").select("Phoenix Feather Portfolio")
+    cy.get("#oneTimeInvestment").type(15000)
+    cy.get("#years").type(12)
+    cy.get("[data-test='create-offer']").click()
+    cy.get("[data-test='customer-name']").type("Jozo")
+    cy.get('[data-test="create-investment"]').click()
 
-        cy.get('ul.investment-list')
-            .find('li')
-            .eq(0)
-            .within(() => {
-                cy.get('h4').should('contain.text', 'Jozo')
-                cy.contains('button', 'View Details').click()
-            })
+    cy.get("ul.investment-list")
+      .find("li")
+      .eq(0)
+      .within(() => {
+        cy.get("h4").should("contain.text", "Jozo")
+        cy.contains("button", "View Details").click()
+      })
 
-        cy.get('[data-test="investment-modal"]')
-            .should('be.visible')
-            .within(() => {
-                cy.get('.your-data')
-                    .contains('p', 'Name')
-                    .find('span')
-                    .should('have.text', 'Jozo')
-            })
-    })
+    cy.get('[data-test="investment-modal"]')
+      .should("be.visible")
+      .within(() => {
+        cy.get(".your-data").contains("p", "Name").find("span").should("have.text", "Jozo")
+      })
+  })
 })
--- cypress/e2e/2.lekcia/02-lekcia-start.cy.js
+++ cypress/e2e/2.lekcia/02-lekcia-start.cy.js
@@ -1,30 +1,29 @@
 describe("Gringottbank", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:8080/#/gringottsBank")
-    })
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/#/gringottsBank")
+  })
 
-    it("display offer data", () => {
-        //TODO:
-        //1. zadat vstupne udaje pre fond, investiciu a roky
-        //2. kliknut na tlacidlo make me an offer
-        //3. overit ze roky a fond sa spravne zobrazili v 
+  it("display offer data", () => {
+    //TODO:
+    //1. zadat vstupne udaje pre fond, investiciu a roky
+    //2. kliknut na tlacidlo make me an offer
+    //3. overit ze roky a fond sa spravne zobrazili v
+    //naucime sa
+    //1. interkciu so selectom, inputom
+    //2. best practices hladania elementov (podla data test atributov,textu)
+    //3. retazenie hladania elementov (hladanie elementu v elemente)
+    //4. pouzitie metody within na hladanie len v konkretnom elemente
+  })
 
-        //naucime sa
-        //1. interkciu so selectom, inputom
-        //2. best practices hladania elementov (podla data test atributov,textu)
-        //3. retazenie hladania elementov (hladanie elementu v elemente)
-        //4. pouzitie metody within na hladanie len v konkretnom elemente
-    })
-
-    it.only("correct name is stored in new investment", () => {
-        //TODO
-        //1. zadajte vstupne parametre (fond, roky, investicia)
-        //2. kliknite na tlacidlo make me an offer
-        //3. zadajte meno
-        //4. kliknite na create investment
-        //5. overte ze sa vytvorila nova investicia
-        //6. v nahlade investice overte spravne meno
-        //7. kliknite na View details
-        //8. overte ze meno je spravne vypisane
-    })
+  it.only("correct name is stored in new investment", () => {
+    //TODO
+    //1. zadajte vstupne parametre (fond, roky, investicia)
+    //2. kliknite na tlacidlo make me an offer
+    //3. zadajte meno
+    //4. kliknite na create investment
+    //5. overte ze sa vytvorila nova investicia
+    //6. v nahlade investice overte spravne meno
+    //7. kliknite na View details
+    //8. overte ze meno je spravne vypisane
+  })
 })
