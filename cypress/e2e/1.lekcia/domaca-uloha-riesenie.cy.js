5dd1dae03ff73731572d0b01322f3332aace383b
michalbanas <banas.michal@icloud.com>
2024-04-10 13:48:59 +0200

    chore: prva lekcia riesenie

--- cypress/e2e/1.lekcia/domaca-uloha-riesenie.cy.js
+++ cypress/e2e/1.lekcia/domaca-uloha-riesenie.cy.js
@@ -2,20 +2,23 @@ describe("Lekcia 1: QUOTE GENERATOR", () => {
   beforeEach(() => {
     cy.visit("/quotes")
   })
-  it("homework", () => {
-    cy.log("Overte hlavný titulok")
+  it("Overte hlavný titulok", () => {
     cy.get("h1").should("be.visible").and("have.text", "Potter Quotes")
+  })
 
-    cy.log("Overte, že po otvorení stránky je button remove quote zablokovaný (disabled)")
+  it("Overte, že po otvorení stránky je button remove quote zablokovaný (disabled)", () => {
     cy.get("#remove-quote").should("be.disabled")
+  })
 
-    cy.log("Overte že button remove quote má správny text")
+  it("Overte že button remove quote má správny text", () => {
     cy.get("#remove-quote").should("contain.text", "Remove Quote")
+  })
 
-    cy.log("Overte že button add quote má správny text")
+  it("Overte že button add quote má správny text", () => {
     cy.get("[data-test='get-quote']").should("be.visible").and("contain.text", "Get Quote")
+  })
 
-    cy.log("Overte 2 quotes")
+  it("Overte 2 quotes", () => {
     cy.get("[data-test='get-quote']").click().click()
     cy.get(".quote-list li").should("have.length", "2")
   })
