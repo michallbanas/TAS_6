d33f36e829ebf33a984a99f3c839ddb0a1865a72
martin <skarbala.martin@gmail.com>
2024-04-18 19:25:39 +0200

    updates lecture start

--- cypress/e2e/4.lekcia/04-lekcia-start.cy.js
+++ cypress/e2e/4.lekcia/04-lekcia-start.cy.js
@@ -7,16 +7,14 @@ describe("testing of search form", () => {
     //1. otvorenie stranky
     //2. zakliknutie cookies buttonu
     //3. overit ze cookies popup nie je viditelny
-    //cy nespadne, lebo sme mu dodali celu URL, cize ignor base url v configu
     //4. overenie ze button explore je visible
     //5. vpisanie destinacie
     //6. vyber destinacie
     //7. overenie ze search button ma zmeneny text na 'Search'
     //8. overenie ze mame zvolenu iba jednu destinaciu
-    //9. overenie ze search button ma spravny attribut href
-    //10. overit substring pre Search button
-    //11. overit URL
-    //12. odkliknut checkbox pouzit alias na element
-    //13. pockat staticky na vysledky
+    //9. overit substring pre Search button
+    //10. overit URL
+    //11. odkliknut checkbox pouzit alias na element
+    //12. pockat staticky na vysledky
   })
 })
