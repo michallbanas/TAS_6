28ab199a0415d60cf6633d919c8899cc54ecc7ab
michalbanas <banas.michal@icloud.com>
2024-04-22 18:43:54 +0200

    chore: 5 lekcia

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("setupStorage", () => {
+    cy.setCookie("__kwc_agreed", "true")
+    cy.setCookie("__kwc_settings", "%7B%22marketing%22%3Atrue%2C%22analytics%22%3Atrue%7D")
+
+    cy.log("Set booking checkbox to false")
+    localStorage.setItem("bookingcom_extension_default", "false")
+})
+
