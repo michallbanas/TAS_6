90b0b42e7bc40e10b84c86f1c2f88f7d4d00dae6
martin <skarbala.martin@gmail.com>
2024-04-25 10:20:46 +0200

    adds lecture 7 start

---
+++ cypress/e2e/7.lekcia/7-lekcia-start.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+// PotterAPI documentation https://documenter.getpostman.com/view/6199862/SzYewFs9
+
+describe('Spells API', () => {
+    it('returns list of spells', () => {
+        // get all spells
+        // check that response body is an array
+        // check array length
+    })
+
+    it('returns a specific spell', () => {
+        // get one specific spell
+        // check exact spell information (effect, type, isUnforgivable)
+
+    })
+
+    it('adds new spell', () => {
+        // add one specific spell
+    })
+})
